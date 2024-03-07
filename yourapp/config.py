d1cd71ef152002c4312fbc22095a82600f840977
Aiken Ong <aiken.ong@u.nus.edu>
2024-03-07 22:53:13 +0800

    third commit

--- yourapp/config.py
+++ yourapp/config.py
@@ -1,7 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = 'your-secret-key'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    # postgres://classpart_user:gixuyR8NOba3b7VE0pXtXmOjsNRonthV@dpg-cnksmj6n7f5s73b200f0-a.singapore-postgres.render.com/classpart
     SQLALCHEMY_TRACK_MODIFICATIONS = False
